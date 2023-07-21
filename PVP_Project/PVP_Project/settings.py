7800045cde05ac48fe3605f0c3d4a332949c2fa0
Saptarshi Mazumder <48704857+SaptarshiMazumder@users.noreply.github.com>
2023-07-21 22:20:39 +0900

    nc

--- PVP_Project/PVP_Project/settings.py
+++ PVP_Project/PVP_Project/settings.py
@@ -122,9 +122,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-STATIC_ROOT = '/home/rajPvp/PVP_Project/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
