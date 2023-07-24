4f4446aa74c39147a0bdbda111996e0dedc1d7f2
Rajarshi Mazumder <r.mazumder@pvp.co.jp>
2023-07-24 10:05:23 +0900

    Updated comments

--- PVP_Project/PVP_students_test_app/views_container/student_views.py
+++ PVP_Project/PVP_students_test_app/views_container/student_views.py
@@ -4,39 +4,37 @@ from ..models import Student, Subject
 from ..forms import Student_form
 
 
-# Handles creation of new students
+# Handles creation and deletion of students
 
 def create_student(request):
-    default_subjects_list= Subject.objects.all()
-    subject_list=[]
-    if request.method=='POST':
-        student_form= Student_form(request.POST)
-        if(student_form.is_valid):
-            
-            instance= student_form.save(commit=True)
+    default_subjects_list = Subject.objects.all()
+    subject_list = []
+    if request.method == 'POST':
+        student_form = Student_form(request.POST)
+        if (student_form.is_valid):
+
+            instance = student_form.save(commit=True)
 
             for sub in default_subjects_list:
-                unverified_marks= request.POST.get(sub.name)
-                verified_marks=0
+                unverified_marks = request.POST.get(sub.name)
+                verified_marks = 0
                 if unverified_marks.isdigit():
-                    verified_marks= int(unverified_marks) if int(unverified_marks) >=0 and int(unverified_marks) <=100 else max(min(int(unverified_marks),100),0)
-                
-                
+                    verified_marks = int(unverified_marks) if int(unverified_marks) >= 0 and int(
+                        unverified_marks) <= 100 else max(min(int(unverified_marks), 100), 0)
+
                 subject_list.append({'subject': sub.name,
-                                         'marks': verified_marks,
-                                         'category':sub.category.name,
-                                         })
-            stud_obj= Student.objects.get(id= instance.id)
-            stud_obj.subjects= subject_list
+                                     'marks': verified_marks,
+                                     'category': sub.category.name,
+                                     })
+            stud_obj = Student.objects.get(id=instance.id)
+            stud_obj.subjects = subject_list
             stud_obj.save()
-            
-    
-    
+
     return redirect('index')
 
 
 def delete_student(request, stud_id):
-    
+
     if request.method == 'POST':
         student_to_delete = Student.objects.get(id=stud_id)
         student_to_delete.delete()
--- PVP_Project/PVP_students_test_app/views_container/subject_views.py
+++ PVP_Project/PVP_students_test_app/views_container/subject_views.py
@@ -3,25 +3,24 @@ from django.http import HttpResponse, JsonResponse
 from ..models import Subject
 from ..forms import Subject_form
 
-#  Handles creation of new subjects
+#  Handles creation and deletion of subjects
+
 
 def create_subject(request):
-    template_name='home_page.html'
-    
-    if request.method=='POST':
-        subject_form= Subject_form(request.POST)
-        if(subject_form.is_valid):
+    template_name = 'home_page.html'
+
+    if request.method == 'POST':
+        subject_form = Subject_form(request.POST)
+        if (subject_form.is_valid):
             subject_form.save()
-            return redirect('index') 
+            return redirect('index')
     return redirect('index')
+
 
 def delete_subject(request, sub_id):
     print(sub_id)
-    if request.method== 'POST':
-        subject_to_delete= Subject.objects.get(id= sub_id)
+    if request.method == 'POST':
+        subject_to_delete = Subject.objects.get(id=sub_id)
         subject_to_delete.delete()
         return redirect('index')
     return redirect('index')
-
-
-
