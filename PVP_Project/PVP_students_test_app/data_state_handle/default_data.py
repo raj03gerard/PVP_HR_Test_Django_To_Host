3a0d8cd4c0f75abf9a1a411b9ae1686e5baa3db5
Rajarshi Mazumder <r.mazumder@pvp.co.jp>
2023-07-24 09:18:29 +0900

    added more default colors

--- PVP_Project/PVP_students_test_app/data_state_handle/default_data.py
+++ PVP_Project/PVP_students_test_app/data_state_handle/default_data.py
@@ -4,16 +4,22 @@ from enum import Enum
 # colors for the different categories( Default_Category_Colors) , etc
 
 
-default_total_passing_marks=300
+default_total_passing_marks = 300
+
 
 class Student_Evaluation_Results(Enum):
-    NOT_EVALUATED= 'NOT_EVALUATED'
-    PASS= 'PASS'
-    FAIL= 'FAIL'
+    NOT_EVALUATED = 'NOT_EVALUATED'
+    PASS = 'PASS'
+    FAIL = 'FAIL'
 
 
 class Default_Category_Colors(Enum):
-    lavender = "#E6E6FA"      
-    powderBlue = "#B0E0E6"    
-    almond = "#FAEBD7"    
-    brown = "#CBBEB5"  
+    lavender = "#E6E6FA"
+    powderBlue = "#B0E0E6"
+    almond = "#FAEBD7"
+    brown = "#CBBEB5"
+    green = "#F2EE9D"
+    lightBlue = "#78C1F3"
+    orange = "#FFA41B"
+    pink = "#F1D4E5"
+    pinkOrange = "#F99B7D"
